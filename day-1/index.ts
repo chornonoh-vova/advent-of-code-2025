310f9111359a37fe38f682bd205335e2e7a3ccea
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2025-12-01 11:01:40 +0200

    Day 1 refactor TS-specifics

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,22 +1,29 @@
 import { readFile } from "node:fs/promises";
-import process from "node:process";
+import { argv, exit } from "node:process";
 
-const filename = process.argv[2];
+const filename = argv[2];
+
+if (!filename) {
+  console.error("expect filename");
+  exit(1);
+}
 
 const instructions = (await readFile(filename, "utf-8")).trim().split("\n");
+
+type Direction = "L" | "R";
 
 class Safe {
   #dial: number;
   part1: number;
   part2: number;
 
-  constructor(initial: number) {
+  constructor(initial = 50) {
     this.#dial = initial;
     this.part1 = 0;
     this.part2 = 0;
   }
 
-  turn(direction: "L" | "R", amount: number) {
+  turn(direction: Direction, amount: number) {
     const dir = direction === "L" ? -1 : 1;
 
     for (let i = 0; i < amount; ++i) {
@@ -34,10 +41,10 @@ class Safe {
   }
 }
 
-const safe = new Safe(50);
+const safe = new Safe();
 
 for (const instruction of instructions) {
-  const direction = instruction.substring(0, 1) as "L" | "R";
+  const direction = instruction.substring(0, 1) as Direction;
   const amount = parseInt(instruction.substring(1));
 
   safe.turn(direction, amount);
